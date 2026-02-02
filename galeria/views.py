8ce74541026a61ab0d7b784b00ffbca70172d93e
Thaís Moreira <thaisdmedeiros@gmail.com>
2026-02-02 10:41:25 +0100

    feat: create view for imagem request

--- galeria/views.py
+++ galeria/views.py
@@ -3,4 +3,8 @@ from django.shortcuts import render
 
 def index(request):
     """Função responsável por responder a uma requisção"""
-    return render(request, "index.html")
+    return render(request, "galeria/index.html")
+
+
+def imagem(request):
+    return render(request, "galeria/imagem.html")
