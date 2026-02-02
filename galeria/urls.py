58392280063ece349b24fbd5c34d4dfe941c39de
Thaís Moreira <thaisdmedeiros@gmail.com>
2026-02-02 11:38:03 +0100

    feat: name index and imagem urlpatterns and aplied in the imagem.html and index.html to django conect the correct path

--- galeria/urls.py
+++ galeria/urls.py
@@ -2,4 +2,4 @@ from django.urls import path
 from galeria.views import index, imagem
 
 # criou para incluir todos os paths - rotas de galeria
-urlpatterns = [path("", index), path("imagem/", imagem)]
+urlpatterns = [path("", index, name="index"), path("imagem/", imagem, name="imagem")]
