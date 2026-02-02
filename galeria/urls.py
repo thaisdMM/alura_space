277e81515848d568b59e7aa5ba3a9a9e9e9b41d8
Thaís Moreira <thaisdmedeiros@gmail.com>
2026-02-02 10:40:54 +0100

    feat: update urls routes to galeria/index and create galeria/imagem route

--- galeria/urls.py
+++ galeria/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from galeria.views import index
+from galeria.views import index, imagem
 
 # criou para incluir todos os paths - rotas de galeria
-urlpatterns = [path("", index)]
+urlpatterns = [path("", index), path("imagem/", imagem)]
