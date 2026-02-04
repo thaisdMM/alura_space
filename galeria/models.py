547c0f653e93b9d6df0f04b40c722aba461c0536
Thaís Moreira <thaisdmedeiros@gmail.com>
2026-02-04 11:28:26 +0100

    feat(models): create fotografia table and aply migrations on database

--- galeria/models.py
+++ galeria/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+# criar classes para virar tabela de banco de dados(sqlite3) usando Django ORM
+#  1- usar um comando no terminal para criar migrations : python manage.py makemigrations
+# 2- usar um comando no terminal para rodar migrations : ppython manage.py migrate
+
+
+class Fotografia(models.Model):
+    # Charfiel - str caracteres
+    # blank = evita str vazia
+    nome = models.CharField(max_length=100, null=False, blank=False)
+    legenda = models.CharField(max_length=150, null=False, blank=False)
+    descricao = models.TextField(null=False, blank=False)
+    # nome da foto
+    foto = models.CharField(max_length=100, null=False, blank=False)
+
+    def __str__(self):
+        return f"Fotografia [nome={self.nome}]"
