2734f4c3e914eb932d7ce4b9b6264c57e859bae9
Thaís Moreira <thaisdmedeiros@gmail.com>
2026-02-04 11:41:45 +0100

    chore: create first data in galeria_fotografia table using shell

--- galeria/models.py
+++ galeria/models.py
@@ -16,3 +16,20 @@ class Fotografia(models.Model):
 
     def __str__(self):
         return f"Fotografia [nome={self.nome}]"
+
+
+# CODIGO NO TERMINAL PARA CRIAR UMA FOTO
+
+# (.venv) ➜  alura-space git:(persistencia-dados-e-admin) python manage.py shell
+# 13 objects imported automatically (use -v 2 for details).
+
+# Cmd click to launch VS Code Native REPL
+# Python 3.13.5 (main, Jun 11 2025, 15:36:57) [Clang 17.0.0 (clang-1700.0.13.3)] on darwin
+# Type "help", "copyright", "credits" or "license" for more information.
+# (InteractiveConsole)
+# >>> from galeria.models import Fotografia
+# >>> foto = Fotografia(nome="Nebulosa de Carina", legenda="webbtelecope.org / NASA / James Webb", foto="carina-nebulosa.png")
+# >>> foto.save()
+# >>> Fotografia.objects.all()
+# <QuerySet [<Fotografia: Fotografia [nome=Nebulosa de Carina]>]>
+# >>>
