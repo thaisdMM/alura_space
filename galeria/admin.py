8f39cf595b62acbfedbfc97062c7f0b9648e5333
Thaís Moreira <thaisdmedeiros@gmail.com>
2026-02-05 11:04:16 +0100

    feat(admin): create admin register in the galeria to use for models Fotografia

--- galeria/admin.py
+++ galeria/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
 
-# Register your models here.
+from galeria.models import Fotografia
+
+
+class ListandoFotografias(admin.ModelAdmin):
+    """Classe para exibir e editar as fotografias no painel do Django Admin"""
+
+    list_display = ("id", "nome", "legenda")
+    # para exibir como link
+    list_display_links = ("id", "nome")
+    # adicionar um campo de busca
+    # - search_fields tem que ser uma tupla só a , já é o suficiente ("nome",)
+    search_fields = ("nome",)
+
+
+# toda vez que criar uma nova classe tem que passar para o admin.site.register
+admin.site.register(Fotografia, ListandoFotografias)
