f04ec735abce6399d7d932464c1e3095176dd802
Thaís Moreira <thaisdmedeiros@gmail.com>
2026-02-05 10:12:09 +0100

    feat: add new image and implemented dinamic visualization in the html using database foto_id

--- galeria/urls.py
+++ galeria/urls.py
@@ -2,4 +2,8 @@ from django.urls import path
 from galeria.views import index, imagem
 
 # criou para incluir todos os paths - rotas de galeria
-urlpatterns = [path("", index, name="index"), path("imagem/", imagem, name="imagem")]
+urlpatterns = [
+    path("", index, name="index"),
+    # /<int:foto_id>: acrescentou para exibir outras fotos pelo id
+    path("imagem/<int:foto_id>", imagem, name="imagem"),
+]
--- galeria/views.py
+++ galeria/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from galeria.models import Fotografia
 
 
@@ -10,6 +10,11 @@ def index(request):
 
     return render(request, "galeria/index.html", {"cards": fotografias})
 
+# vai receber o foto_id para fazer referencia ao id do banco de dados
+def imagem(request, foto_id):
 
-def imagem(request):
-    return render(request, "galeria/imagem.html")
+    #  Passa o model, e do objeto a pk(primary_key)= foto_id
+    fotografia = get_object_or_404(Fotografia, pk=foto_id)
+
+    # esse dicionario é para passar para o imagem.html o objeto que faz referencia ao id
+    return render(request, "galeria/imagem.html", {"fotografia": fotografia})
