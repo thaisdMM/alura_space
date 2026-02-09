4e40a67f99e0fa5fbd62de0f7cbb8291e365470f
Thaís Moreira <thaisdmedeiros@gmail.com>
2026-02-09 18:53:05 +0100

    feat: create new app usuarios with minors config to setup -installed_apps and urlpatterns

--- setup/settings.py
+++ setup/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "galeria.apps.GaleriaConfig",
+    "usuarios.apps.UsuariosConfig",
 ]
 
 MIDDLEWARE = [
--- setup/urls.py
+++ setup/urls.py
@@ -24,6 +24,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("galeria.urls")),
+    path("", include("usuarios.urls")),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 # Serve static files during development
