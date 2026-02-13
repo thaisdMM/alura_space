4c252a58191095a8efe37ac8f9e19b0dea297f79
Thaís Moreira <thaisdmedeiros@gmail.com>
2026-02-13 19:05:36 +0100

    feat: create forms.py and added to login views

--- usuarios/views.py
+++ usuarios/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
 
+from usuarios.forms import LoginForms
+
 
 def login(request):
-    return render(request, "usuarios/login.html")
+    # instanciação de objeto formulario
+    form = LoginForms()
+    return render(request, "usuarios/login.html", {"form": form})
 
 
 def cadastro(request):
