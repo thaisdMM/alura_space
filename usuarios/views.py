40b9870a224a8de36689876d097ee1c99b08cc6c
Thaís Moreira <thaisdmedeiros@gmail.com>
2026-02-24 18:51:11 +0100

    feat: implement logout view, add to urlpatterns, and add url logout to login, cadastro and _menu templates

--- usuarios/views.py
+++ usuarios/views.py
@@ -106,3 +106,9 @@ def cadastro(request):
             return redirect("login")
 
     return render(request, "usuarios/cadastro.html", {"form": cadastro_form})
+
+
+def logout(request):
+    auth.logout(request)
+    messages.success(request, "Logout efetuado com sucesso!")
+    return redirect("login")
